82497a2cedd59430614f35e952d33de42ff6fae8
Andrey Sandler <mrdreem13@gmail.com>
2024-05-08 20:13:46 +0300

    Create button in ViewController

--- surfBegginer/ViewController.swift
+++ surfBegginer/ViewController.swift
@@ -10,11 +10,16 @@ import UIKit
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
+        
+        // MARK: - Override methods
         super.viewDidLoad()
         
         
         // MARK: -  Greeting refactoring
+        let button = UIButton()
         let label = UILabel()
+        
+        button.tintColor = .darkGray
         label.text = "Hello, surfer's"
         view.addSubview(label)
     }
